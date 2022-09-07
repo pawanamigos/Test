9198f165588b166c3084debb769082194379dfc5
pawanamigos <44724721+pawanamigos@users.noreply.github.com>
2022-09-08 07:35:17 +1000

    Update authorizer.py
    Created and env variable with an assigned value and called the env variable to fix COGNITO_POOL_ARN key error.

--- chalicelib/authorizer.py
+++ chalicelib/authorizer.py
@@ -1,8 +1,12 @@
+import os
 from os import environ
 
 from chalice.app import CognitoUserPoolAuthorizer
 
+os.environ["COGNITO_POOL_ARN"] = "COGNITO_POOL_ARN"
+
+
 cognito_authorizer = CognitoUserPoolAuthorizer(
     "CognitoAuthorizer",
-    provider_arns=[environ["COGNITO_POOL_ARN"]],
+    provider_arns=[os.getenv("COGNITO_POOL_ARN")],
 )
